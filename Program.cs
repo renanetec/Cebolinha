24ea1bb14ab8ae5e31e4efd5688ae712fe737965
renanetec <renanmenezescarlos21@gmail.com>
2021-03-21 17:28:36 -0300

    altera tudo

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace cebolinha
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+          
+            Console.Clear();
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            string boasvindas = "Bem Vindo!";
+            string explicação = "digite aqui:O rato roeu a roupa do rei de roma";
+            //digite acima:O rato roeu a roupa do rei de roma
+            //salve, e depois execute no terminal o comando "dotnet run"
+            string mensagem = $@"{boasvindas}
+            
+            {explicação}.";
+
+            string novaMensagem = mensagem.Replace("r", "l");
+
+            Console.WriteLine(novaMensagem);
+            Console.ResetColor();
+
+        }
+    }
+}
